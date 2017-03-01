df2cc3139d6d1cdee4b216fd34d77c8e7ce36c57
ProBucket <hasan.khan.pro@gmail.com>
2017-03-01 13:11:37 +0600

    Update C#.cs

--- C#.cs
+++ C#.cs
@@ -1,8 +1,41 @@
+// C# code demo
+using System;
+namespace HelloWorldApplication
+{
+   class HelloWorld
+   {
+      static void Main(string[] args)
+      {
+         /* my first program in C# */
+         Console.WriteLine("Hello World");
+         Console.ReadKey();
+      }
+   }
+}
 
-/**
-*   Create List
-*/
+// Print
+Console.WriterLine("Welcome to c#");
 
+// Create List
+List<string> strList = new List<string>();
 List<string> strList = new List<string>(new string[] {"Tom", "Hasan"});
+
+// String sorting
+strList.Sort();
+
+// Exception handle
+try{
+  Console.Write("Divide 10 By");
+  int num = int.Parse(Console.ReadLine());
+  Console.WriteLine("10 / {0} = {1}",num, (10/num));
+} catch(DivideByZeroException ex){
+  Console.WriteLine("Cant divide by zero");
+  Console.WriteLine(ex.getType().Name);
+  Console.WriteLine(ex.Message);
+} catch(Exception ex){
+  Console.WriteLine(ex.getType().Name);
+  Console.WriteLine(ex.Message);
+}
   
+
 
