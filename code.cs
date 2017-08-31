67934a2b8432006fd347af0209fe868f5c16f069
ProBucket <hasan.khan.pro@gmail.com>
2017-08-31 09:46:03 +0600

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Methods returning string
+            string[] sarray = { "Hello", "From", "Tutorials", "Point" };
+            string message = String.Join(" ", sarray);
+            Console.WriteLine("Message: {0}", message);
+
+            // Formatting method to convert a value
+            DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
+            string chat = String.Format("Message sent at {0:t} on {0:D}", waiting);
+            Console.WriteLine("Message: {0}", chat);
+
+
+            Console.ReadKey();
+        }
+    }
+}
